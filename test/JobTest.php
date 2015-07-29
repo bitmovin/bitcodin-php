397fe7050fc51b955add8ca9588b2f9efa962c2c
Dominic Weinberger <dominic.weinberger@bitmovin.net>
2015-07-29 15:35:26 +0200

    Added a test for creating a widevine drm job

--- test/JobTest.php
+++ test/JobTest.php
@@ -22,10 +22,64 @@ use bitcodin\EncodingProfileConfig;
 use bitcodin\JobConfig;
 use bitcodin\Output;
 use bitcodin\S3OutputConfig;
+use bitcodin\WidevineDRMConfig;
+use bitcodin\DRMEncryptionMethods;
+use bitcodin\JobSpeedTypes;
 
 class JobTest extends BitcodinApiTestBaseClass {
 
     const URL_FILE = 'http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkv';
+
+    public function testCreateDRMJob()
+    {
+        $inputConfig = new HttpInputConfig();
+        $inputConfig->url = self::URL_FILE;
+        $input = Input::create($inputConfig);
+
+
+        /* CREATE VIDEO STREAM CONFIG */
+        $videoStreamConfig = new VideoStreamConfig();
+        $videoStreamConfig->bitrate = 1024000;
+        $videoStreamConfig->height = 480;
+        $videoStreamConfig->width = 202;
+
+
+        /* CREATE AUDIO STREAM CONFIGS */
+        $audioStreamConfig = new AudioStreamConfig();
+        $audioStreamConfig->bitrate = 256000;
+
+        $encodingProfileConfig = new EncodingProfileConfig();
+        $encodingProfileConfig->name = 'MyApiTestEncodingProfile';
+        $encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
+        $encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
+
+        /* CREATE ENCODING PROFILE */
+        $encodingProfile = EncodingProfile::create($encodingProfileConfig);
+
+        /* CREATE DRM WIDEVINE CONFIG */
+        $widevineDRMConfig = new WidevineDRMConfig();
+        $widevineDRMConfig->requestUrl = 'http://license.uat.widevine.com/cenc/getcontentkey';
+        $widevineDRMConfig->signingKey = '1ae8ccd0e7985cc0b6203a55855a1034afc252980e970ca90e5202689f947ab9';
+        $widevineDRMConfig->signingIV = 'd58ce954203b7c9a9a9d467f59839249';
+        $widevineDRMConfig->contentId = '746573745f69645f4639465043304e4f';
+        $widevineDRMConfig->provider = 'widevine_test';
+        $widevineDRMConfig->method = DRMEncryptionMethods::MPEG_CENC;
+
+        $jobConfig = new JobConfig();
+        $jobConfig->encodingProfile = $encodingProfile;
+        $jobConfig->input = $input;
+        $jobConfig->manifestTypes[] = ManifestTypes::MPD;
+        $jobConfig->speed = JobSpeedTypes::STANDARD;
+        $jobConfig->drmConfig = $widevineDRMConfig;
+
+        /* CREATE JOB */
+        $job = Job::create($jobConfig);
+
+        $this->assertInstanceOf('bitcodin\Job', $job);
+        $this->assertNotNull($job->jobId);
+        $this->assertNotEquals($job->status, Job::STATUS_ERROR);
+        return $job;
+    }
 
     public function testCreateJob()
     {
