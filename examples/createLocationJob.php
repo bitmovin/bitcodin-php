615bda34cfde5342129ddaafb54b60cbe7bf9dc1
David Moser <david.moser@perau.at>
2015-08-20 15:37:52 +0200

    createLocationJob.php: New example createLocationJob.php to create a Job with location provided JobConfig.php: Added field location

---
+++ examples/createLocationJob.php
@@ -0,0 +1,74 @@
+<?php
+
+use bitcodin\Bitcodin;
+use bitcodin\VideoStreamConfig;
+use bitcodin\AudioStreamConfig;
+use bitcodin\Job;
+use bitcodin\JobConfig;
+use bitcodin\Input;
+use bitcodin\HttpInputConfig;
+use bitcodin\EncodingProfile;
+use bitcodin\EncodingProfileConfig;
+use bitcodin\ManifestTypes;
+use bitcodin\Output;
+use bitcodin\FtpOutputConfig;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+/* CONFIGURATION */
+Bitcodin::setApiToken('insertYourApiKey'); // Your can find your api key in the settings menu. Your account (right corner) -> Settings -> API
+
+$inputConfig = new HttpInputConfig();
+$inputConfig->url = 'http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkv';
+$input = Input::create($inputConfig);
+
+/* CREATE VIDEO STREAM CONFIG */
+$videoStreamConfig = new VideoStreamConfig();
+$videoStreamConfig->bitrate = 1024000;
+$videoStreamConfig->height = 480;
+$videoStreamConfig->width = 202;
+
+/* CREATE AUDIO STREAM CONFIGS */
+$audioStreamConfig = new AudioStreamConfig();
+$audioStreamConfig->bitrate = 256000;
+
+$encodingProfileConfig = new EncodingProfileConfig();
+$encodingProfileConfig->name = 'MyApiTestEncodingProfile';
+$encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
+$encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
+
+/* CREATE ENCODING PROFILE */
+$encodingProfile = EncodingProfile::create($encodingProfileConfig);
+
+$jobConfig = new JobConfig();
+$jobConfig->encodingProfile = $encodingProfile;
+$jobConfig->input = $input;
+$jobConfig->manifestTypes[] = ManifestTypes::M3U8;
+$jobConfig->location = "europe";
+
+/* CREATE JOB */
+$job = Job::create($jobConfig);
+
+/* WAIT TIL JOB IS FINISHED */
+do{
+    $job->update();
+    sleep(1);
+} while($job->status != Job::STATUS_FINISHED && $job->status != Job::STATUS_ERROR);
+
+
+$outputConfig = new FtpOutputConfig();
+$outputConfig->name = "TestS3Output";
+$outputConfig->host = str_replace('ftp://', '', getKey('ftpServer'));
+$outputConfig->username = getKey('ftpUser');
+$outputConfig->password = getKey('ftpPassword');
+
+$output = Output::create($outputConfig);
+
+/* TRANSFER JOB OUTPUT */
+$job->transfer($output);
+
+/* HELPER FUNCTION */
+function getKey($key)
+{
+    return json_decode(file_get_contents(__DIR__.'/../test/config.json'))->{$key};
+}
