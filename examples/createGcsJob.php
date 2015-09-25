5b615f603e6824d880fff4eea07da5897cbe2141
Gernot Zwantschko <gernot.zwantschko@bitmovin.net>
2015-09-25 11:18:17 +0200

    createGcsJob.php: Replaced S3Input with HTTP Input for simplicity

--- examples/createGcsJob.php
+++ examples/createGcsJob.php
@@ -13,25 +13,20 @@ use bitcodin\AudioStreamConfig;
 use bitcodin\Job;
 use bitcodin\JobConfig;
 use bitcodin\Input;
-use bitcodin\S3InputConfig;
 use bitcodin\EncodingProfile;
 use bitcodin\EncodingProfileConfig;
 use bitcodin\ManifestTypes;
 use bitcodin\Output;
 use bitcodin\GcsOutputConfig;
+use bitcodin\HttpInputConfig;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 /* CONFIGURATION */
 Bitcodin::setApiToken('insertYourApiKey'); // Your can find your api key in the settings menu. Your account (right corner) -> Settings -> API
 
-$inputConfig = new S3InputConfig();
-$inputConfig->accessKey = 'yourAWSAccessKey';
-$inputConfig->secretKey = 'yourAWSSecretKey';
-$inputConfig->bucket    = 'yourBucketName';
-$inputConfig->region    = AwsRegion::EU_WEST_1;             // bucket region
-$inputConfig->objectKey = 'path/to/your/fileonbucket.mp4';
-$inputConfig->host      = 's3-eu-west-1.amazonaws.com';      // OPTIONAL
+$inputConfig = new \bitcodin\HttpInputConfig();
+$inputConfig->url = "http://eu-storage.bitcodin.com/inputs/Sample-Input-Video.mkv";
 $input = Input::create($inputConfig);
 
 /* CREATE OUTPUT CONFIG  */
