cf11a3586ce9013b73c2ee1ca52568785d8eda5b
Gernot Zwantschko <gernot.zwantschko@bitmovin.net>
2015-09-29 11:22:38 +0200

    createOutputTransferJob.php: Alignment

--- examples/createOutputTransferJob.php
+++ examples/createOutputTransferJob.php
@@ -20,16 +20,16 @@ use bitcodin\ManifestTypes;
 use bitcodin\Output;
 use bitcodin\FtpOutputConfig;
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 /* CONFIGURATION */
 Bitcodin::setApiToken('566a1bc718ecd0deacbea17d4cb2cf9fdf3a57cc19498e90a06c77d44d28104d'); // Your can find your api key in the settings menu. Your account (right corner) -> Settings -> API
 
 $inputConfig = new HttpInputConfig();
-$inputConfig->url = 'http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkvhttp://bitbucketireland.s3.amazonaws.com/Sintel-original-short.mkv';
+$inputConfig->url = 'http://eu-storage.bitcodin.com/inputs/Sintel-original-short.mkv';
 $input = Input::create($inputConfig);
 echo "Input successfully created! \n";
-echo json_encode($input)."\n";
+echo json_encode($input) . "\n";
 
 /* CREATE VIDEO STREAM CONFIG */
 $videoStreamConfig1 = new VideoStreamConfig();
@@ -58,24 +58,24 @@ $audioStreamConfig->bitrate = 128000;
 
 /* CREATE ENCODING PROFILE */
 $encodingProfileConfig = new EncodingProfileConfig();
-$encodingProfileConfig->name = 'ftp Test Profile '.time();
+$encodingProfileConfig->name = 'ftp Test Profile ' . time();
 $encodingProfileConfig->videoStreamConfigs = [$videoStreamConfig1, $videoStreamConfig2, $videoStreamConfig3, $videoStreamConfig4];
 $encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
 $encodingProfile = EncodingProfile::create($encodingProfileConfig);
 echo "Encoding-Profile successfully created! \n";
-echo json_encode($encodingProfile)."\n";
+echo json_encode($encodingProfile) . "\n";
 
 /* CREATE OUTPUT */
 
 $outputConfig = new FtpOutputConfig();
-$outputConfig->name = "FTP Campus Test ".time();
+$outputConfig->name = "FTP Campus Test " . time();
 $outputConfig->host = getKey('ftpServer');
 $outputConfig->username = getKey('ftpUser');
 $outputConfig->password = getKey('ftpPassword');
 
 $output = Output::create($outputConfig);
 echo "Output successfully created! \n";
-echo json_encode($output)."\n";
+echo json_encode($output) . "\n";
 
 $jobConfig = new JobConfig();
 $jobConfig->encodingProfile = $encodingProfile;
@@ -90,11 +90,11 @@ $job = Job::create($jobConfig);
 echo "\n\nCreate Encoding...\n\n";
 
 /* WAIT TIL JOB IS FINISHED */
-do{
+do {
     $job->update();
     echo "\r" . date_create()->format('d.m.Y H:i:s') . ' - Job: ' . $job->jobId . ' Status[' . $job->status . "]";
     sleep(2);
-} while($job->status != Job::STATUS_FINISHED && $job->status != Job::STATUS_ERROR);
+} while ($job->status != Job::STATUS_FINISHED && $job->status != Job::STATUS_ERROR);
 
 echo "\n\nWait for Transfer...\n\n";
 
@@ -104,10 +104,9 @@ do {
     try {
         $transfers = $job->getTransfers();
         $finishedTransfer = 0;
-        foreach($transfers as $transfer)
-        {
-            echo $date . ' - Transfer: JobID ' . $transfer->id . ' Progress[' . $transfer->progress . "] Status[".$transfer->status."]";
-            if($transfer->progress == 100)
+        foreach ($transfers as $transfer) {
+            echo $date . ' - Transfer: JobID ' . $transfer->id . ' Progress[' . $transfer->progress . "] Status[" . $transfer->status . "]";
+            if ($transfer->progress == 100)
                 $finishedTransfer++;
         }
         sleep(2);
@@ -118,17 +117,17 @@ do {
     } catch (\Exception $e) {
         echo "Unexpected Error\n";
     }
-} while(empty($transfers) || $finishedTransfer < count($transfers));
+} while (empty($transfers) || $finishedTransfer < count($transfers));
 
 echo "\n\nTransfer finished...\n\n";
 
-foreach($transfers as $transfer) {
-    echo $transfer->createdAt . ' - Transfer: JobID ' . $transfer->id . ' URL: ' . $transfer->outputProfile->outputUrl."\n";
+foreach ($transfers as $transfer) {
+    echo $transfer->createdAt . ' - Transfer: JobID ' . $transfer->id . ' URL: ' . $transfer->outputProfile->outputUrl . "\n";
 }
 
 
 /* HELPER FUNCTION */
 function getKey($key)
 {
-    return json_decode(file_get_contents(__DIR__.'/../test/config.json'))->{$key};
+    return json_decode(file_get_contents(__DIR__ . '/../test/config.json'))->{$key};
 }
