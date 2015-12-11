d38db642fa69e703178f143e79ce3273cad743c5
Dominic Weinberger <dominic.weinberger@bitmovin.net>
2015-12-11 16:03:25 +0100

    Added getManifestInfo call to Job.php and created an example.

---
+++ examples/createJobAndGetManifestInfo.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doweinberger
+ * Date: 11.12.15
+ * Time: 15:50
+ */
+
+use bitcodin\Bitcodin;
+use bitcodin\JobSpeedTypes;
+use bitcodin\VideoStreamConfig;
+use bitcodin\AudioStreamConfig;
+use bitcodin\Job;
+use bitcodin\JobConfig;
+use bitcodin\Input;
+use bitcodin\HttpInputConfig;
+use bitcodin\EncodingProfile;
+use bitcodin\EncodingProfileConfig;
+use bitcodin\ManifestTypes;
+use bitcodin\Output;
+use bitcodin\FtpOutputConfig;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+/* CONFIGURATION */
+Bitcodin::setApiToken('insertYourApiKey'); // Your can find your api key in the settings menu. Your account (right corner) -> Settings -> API
+
+$inputConfig = new HttpInputConfig();
+$inputConfig->url = 'http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkv';
+$input = Input::create($inputConfig);
+
+/* CREATE VIDEO STREAM CONFIG */
+$videoStreamConfig = new VideoStreamConfig();
+$videoStreamConfig->bitrate = 512000;
+$videoStreamConfig->height = 202;
+$videoStreamConfig->width = 480;
+
+/* CREATE AUDIO STREAM CONFIGS */
+$audioStreamConfig = new AudioStreamConfig();
+$audioStreamConfig->bitrate = 128000;
+
+$encodingProfileConfig = new EncodingProfileConfig();
+$encodingProfileConfig->name = 'MyApiTestEncodingProfile';
+$encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
+$encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
+
+/* CREATE ENCODING PROFILE */
+$encodingProfile = EncodingProfile::create($encodingProfileConfig);
+
+$jobConfig = new JobConfig();
+$jobConfig->encodingProfile = $encodingProfile;
+$jobConfig->input = $input;
+$jobConfig->manifestTypes[] = ManifestTypes::M3U8;
+$jobConfig->manifestTypes[] = ManifestTypes::MPD;
+$jobConfig->speed = JobSpeedTypes::STANDARD;
+
+/* CREATE JOB */
+$job = Job::create($jobConfig);
+
+/* WAIT TIL JOB IS FINISHED */
+do{
+    $job->update();
+    sleep(1);
+} while($job->status != Job::STATUS_FINISHED && $job->status != Job::STATUS_ERROR);
+
+$manifestInfo = Job::getManifestInfo($job->jobId);
+var_dump($manifestInfo);
+
+
+$outputConfig = new FtpOutputConfig();
+$outputConfig->name = "TestS3Output";
+$outputConfig->host = str_replace('ftp://', '', getKey('ftpServer'));
+$outputConfig->username = getKey('ftpUser');
+$outputConfig->password = getKey('ftpPassword');
+
+$output = Output::create($outputConfig);
+
+/* TRANSFER JOB OUTPUT */
+$job->transfer($output);
+
+/* HELPER FUNCTION */
+function getKey($key)
+{
+    return json_decode(file_get_contents(__DIR__.'/../test/config.json'))->{$key};
+}
