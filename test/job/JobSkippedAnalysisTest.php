677eda1fd415fba18c4767b9b8b6e09323249cd6
David Moser <david.moser@perau.at>
2016-02-19 15:42:29 +0100

    Fixed JobSkippedAnalysisTest.php

--- test/job/JobSkippedAnalysisTest.php
+++ test/job/JobSkippedAnalysisTest.php
@@ -9,16 +9,22 @@ namespace test\job;
 
 
 use bitcodin\AudioMetaData;
+use bitcodin\AudioStreamConfig;
 use bitcodin\Bitcodin;
+use bitcodin\EncodingProfile;
+use bitcodin\EncodingProfileConfig;
 use bitcodin\HttpInputConfig;
 use bitcodin\Input;
 use bitcodin\Job;
 use bitcodin\JobConfig;
 use bitcodin\JobSpeedTypes;
 use bitcodin\ManifestTypes;
+use bitcodin\VideoStreamConfig;
 
 class JobSkippedAnalysisTest extends AbstractJobTest
 {
+    const URL_FILE = 'http://bitbucketireland.s3.amazonaws.com/Sintel-original-short.mkv';
+
     public function __construct()
     {
         parent::__construct();
@@ -45,8 +51,24 @@ class JobSkippedAnalysisTest extends AbstractJobTest
         $audioMetaDataSoundAndVoice->label = 'Sound and Voice';
         $audioMetaDataSoundAndVoice->language = 'en';
 
+        /* CREATE VIDEO STREAM CONFIG */
+        $videoStreamConfig = new VideoStreamConfig();
+        $videoStreamConfig->bitrate = 2400000;
+        $videoStreamConfig->height = 720;
+        $videoStreamConfig->width = 1280;
+
+
+        /* CREATE AUDIO STREAM CONFIGS */
+        $audioStreamConfig = new AudioStreamConfig();
+        $audioStreamConfig->bitrate = 256000;
+
+        $encodingProfileConfig = new EncodingProfileConfig();
+        $encodingProfileConfig->name = 'SkippedAnalysisProfile';
+        $encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
+        $encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
+
         /* CREATE ENCODING PROFILE */
-        $encodingProfile = $this->getMultiLanguageEncodingProfile();
+        $encodingProfile = EncodingProfile::create($encodingProfileConfig);
 
         $jobConfig = new JobConfig();
         $jobConfig->encodingProfile = $encodingProfile;
