163f4b4d6919b945866cf750042915bf843a1022
David Moser <david.moser@perau.at>
2016-05-23 14:08:28 +0200

    Added JobHttpsTest.php Updated composer.lock

---
+++ test/job/JobHttpsTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmoser
+ * Date: 23.05.2016
+ * Time: 13:12
+ */
+
+namespace test\job;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+
+use bitcodin\Bitcodin;
+use bitcodin\Input;
+use bitcodin\VideoStreamConfig;
+use bitcodin\AudioStreamConfig;
+use bitcodin\EncodingProfile;
+use bitcodin\ManifestTypes;
+use bitcodin\Job;
+use bitcodin\HttpInputConfig;
+use bitcodin\EncodingProfileConfig;
+use bitcodin\JobConfig;
+
+
+class JobHttpsTest extends AbstractJobTest {
+
+    const URL_FILE = 'http://bitbucketireland.s3.amazonaws.com/Sintel-original-short.mkv';
+
+    public function __construct() {
+        parent::__construct();
+
+        Bitcodin::setApiToken($this->getApiKey());
+    }
+
+    /** TEST FAST-JOB CREATION */
+
+    /**
+     * @test
+     * @return Job
+     */
+    public function createJob()
+    {
+        Bitcodin::setApiToken($this->getApiKey());
+        Bitcodin::enableHttps();
+
+        $inputConfig = new HttpInputConfig();
+        $inputConfig->url = self::URL_FILE;
+        $input = Input::create($inputConfig);
+
+        /* CREATE VIDEO STREAM CONFIG */
+        $videoStreamConfig = new VideoStreamConfig();
+        $videoStreamConfig->bitrate = 1024000;
+        $videoStreamConfig->height = 202;
+        $videoStreamConfig->width = 480;
+
+        /* CREATE AUDIO STREAM CONFIGS */
+        $audioStreamConfig = new AudioStreamConfig();
+        $audioStreamConfig->bitrate = 256000;
+
+        $encodingProfileConfig = new EncodingProfileConfig();
+        $encodingProfileConfig->name = $this->getName().'EncodingProfile';
+        $encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
+        $encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
+
+        /* CREATE ENCODING PROFILE */
+        $encodingProfile = EncodingProfile::create($encodingProfileConfig);
+
+        $jobConfig = new JobConfig();
+        $jobConfig->encodingProfile = $encodingProfile;
+        $jobConfig->input = $input;
+        $jobConfig->manifestTypes[] = ManifestTypes::M3U8;
+        $jobConfig->manifestTypes[] = ManifestTypes::MPD;
+
+        /* CREATE JOB */
+        $job = Job::create($jobConfig);
+
+        $this->assertInstanceOf('bitcodin\Job', $job);
+        $this->assertNotNull($job->jobId);
+        $this->assertNotEquals($job->status, Job::STATUS_ERROR);
+        return $job;
+    }
+}
