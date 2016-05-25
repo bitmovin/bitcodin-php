461940501b19ca94bf07e1c8b8a2a2f0b9359bdf
Gernot Zwantschko <gernot.zwantschko@bitmovin.net>
2016-05-25 10:14:35 +0200

    EncodingProfileCroppingTest.php: Refactored

--- test/encodingProfile/EncodingProfileCroppingTest.php
+++ test/encodingProfile/EncodingProfileCroppingTest.php
@@ -1,132 +1,74 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cwioro
- * Date: 22.06.15
- * Time: 13:57
- */
 
-namespace test\encodingprofile;
+    namespace test\encodingprofile;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+    require_once __DIR__ . '/../../vendor/autoload.php';
 
 
-use bitcodin\Bitcodin;
-use bitcodin\EncodingProfile;
-use bitcodin\VideoStreamConfig;
-use bitcodin\AudioStreamConfig;
-use bitcodin\EncodingProfileConfig;
-use bitcodin\CroppingConfig;
-use test\BitcodinApiTestBaseClass;
+    use bitcodin\AudioStreamConfig;
+    use bitcodin\CroppingConfig;
+    use bitcodin\EncodingProfile;
+    use bitcodin\EncodingProfileConfig;
+    use bitcodin\VideoStreamConfig;
+    use test\BitcodinApiTestBaseClass;
 
 
-class EncodingCroppingProfileTest extends BitcodinApiTestBaseClass {
-
-    public function __construct() {
-        parent::__construct();
-
-        Bitcodin::setApiToken($this->getApiKey());
-    }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createCroppingConfigWithNegativeTop()
+    class EncodingProfileCroppingTest extends BitcodinApiTestBaseClass
     {
-        Bitcodin::setApiToken($this->getApiKey());
 
-        $croppingConfig = new CroppingConfig();
-        $croppingConfig->top = -10;
-        $croppingConfig->bottom = 2;
-        $croppingConfig->right = 0;
-        $croppingConfig->left = 10;
+        public function setUp()
+        {
+            parent::setUp();
+        }
 
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->croppingConfig = $croppingConfig;
+        /**
+         * @test
+         * @dataProvider encodingProfileProvider
+         * @expectedException               \bitcodin\exceptions\BitcodinException
+         */
+        public function createEncodingProfileWithInvalidCroppingConfig(EncodingProfileConfig $encodingProfileConfig)
+        {
+            EncodingProfile::create($encodingProfileConfig);
+        }
 
-        EncodingProfile::create($encodingProfileConfig);
+        public function encodingProfileProvider()
+        {
+            $encodingProfileConfigs = array();
+            $croppingConfigs = array(
+                'croppingWithNegativeTop'   => new CroppingConfig(-10, 2, 10, 0),
+                'croppingWithNegativeRight' => new CroppingConfig(10, 2, 10, -1),
+                'croppingWithNegativeLeft'  => new CroppingConfig(10, 2, -10, 1)
+            );
+
+            foreach ($croppingConfigs as $name => $croppingConfig) {
+                $encodingProfileConfig = $this->getEncodingProfileConfig();
+                $encodingProfileConfig->croppingConfig = $croppingConfig;
+
+                $encodingProfileConfigs[$name] = array( $encodingProfileConfig );
+            }
+
+            return $encodingProfileConfigs;
+        }
+
+        private function getEncodingProfileConfig()
+        {
+            /* CREATE VIDEO STREAM CONFIG */
+            $videoStreamConfig = new VideoStreamConfig();
+            $videoStreamConfig->bitrate = 1024000;
+            $videoStreamConfig->height = 202;
+            $videoStreamConfig->width = 480;
+
+            /* CREATE AUDIO STREAM CONFIGS */
+            $audioStreamConfig = new AudioStreamConfig();
+            $audioStreamConfig->bitrate = 256000;
+
+            $encodingProfileConfig = new EncodingProfileConfig();
+            $encodingProfileConfig->name = $this->getName() . 'EncodingProfile';
+            $encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
+            $encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
+
+            return $encodingProfileConfig;
+        }
+
+
     }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createCroppingConfigWithNegativeRightValue()
-    {
-        Bitcodin::setApiToken($this->getApiKey());
-
-        $croppingConfig = new CroppingConfig();
-        $croppingConfig->top = 10;
-        $croppingConfig->bottom = 2;
-        $croppingConfig->right = -1;
-        $croppingConfig->left = 10;
-
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->croppingConfig = $croppingConfig;
-
-        EncodingProfile::create($encodingProfileConfig);
-    }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createCroppingConfigWithNegativeLeftValue()
-    {
-        Bitcodin::setApiToken($this->getApiKey());
-
-        $croppingConfig = new CroppingConfig();
-        $croppingConfig->top = 10;
-        $croppingConfig->bottom = 2;
-        $croppingConfig->right = 1;
-        $croppingConfig->left = -10;
-
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->croppingConfig = $croppingConfig;
-
-        EncodingProfile::create($encodingProfileConfig);
-    }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createCroppingConfigWithNegativeBottom()
-    {
-        Bitcodin::setApiToken($this->getApiKey());
-
-        $croppingConfig = new CroppingConfig();
-        $croppingConfig->top = 10;
-        $croppingConfig->bottom = -2;
-        $croppingConfig->right = 1;
-        $croppingConfig->left = 10;
-
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->croppingConfig = $croppingConfig;
-
-        EncodingProfile::create($encodingProfileConfig);
-    }
-
-    private function getEncodingProfileConfigTemplate()
-    {
-        /* CREATE VIDEO STREAM CONFIG */
-        $videoStreamConfig = new VideoStreamConfig();
-        $videoStreamConfig->bitrate = 1024000;
-        $videoStreamConfig->height = 202;
-        $videoStreamConfig->width = 480;
-
-        /* CREATE AUDIO STREAM CONFIGS */
-        $audioStreamConfig = new AudioStreamConfig();
-        $audioStreamConfig->bitrate = 256000;
-
-        $encodingProfileConfig = new EncodingProfileConfig();
-        $encodingProfileConfig->name = $this->getName().'EncodingProfile';
-        $encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
-        $encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
-
-        return $encodingProfileConfig;
-    }
-
-
-}
