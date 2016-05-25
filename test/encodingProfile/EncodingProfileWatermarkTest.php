1adc7929e7161a4e7dc6f349ee6a03ce95b4e1f0
Gernot Zwantschko <gernot.zwantschko@bitmovin.net>
2016-05-25 10:58:35 +0200

    EncodingProfileWatermarkTest.php: Refactored

--- test/encodingProfile/EncodingProfileWatermarkTest.php
+++ test/encodingProfile/EncodingProfileWatermarkTest.php
@@ -1,154 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cwioro
- * Date: 22.06.15
- * Time: 13:57
- */
 
-namespace test\encodingprofile;
+    namespace test\encodingprofile;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+    require_once __DIR__ . '/../../vendor/autoload.php';
 
-use bitcodin\Bitcodin;
-use bitcodin\EncodingProfile;
-use bitcodin\VideoStreamConfig;
-use bitcodin\AudioStreamConfig;
-use bitcodin\EncodingProfileConfig;
-use bitcodin\WatermarkConfig;
-use test\BitcodinApiTestBaseClass;
-use bitcodin\exceptions\BitcodinException;
+    use bitcodin\EncodingProfile;
+    use bitcodin\EncodingProfileConfig;
+    use bitcodin\WatermarkConfig;
+    use test\input\AbstractEncodingProfileTest;
 
-class EncodingProfileWatermarkTest extends BitcodinApiTestBaseClass {
-
-    public function __construct() {
-        parent::__construct();
-
-        Bitcodin::setApiToken($this->getApiKey());
-    }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createWatermarkConfigWithNegativeTop()
+    class EncodingProfileWatermarkTest extends AbstractEncodingProfileTest
     {
-        Bitcodin::setApiToken($this->getApiKey());
 
-        $watermarkConfig = new WatermarkConfig();
-        $watermarkConfig->top = -10;
+        public function setUp()
+        {
+            parent::setUp();
+        }
 
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->watermarkConfig = $watermarkConfig;
+        /**
+         * @test
+         * @dataProvider encodingProfileWithWatermarkConfigProvider
+         * @expectedException \bitcodin\exceptions\BitcodinException
+         *
+         * @param EncodingProfileConfig $encodingProfileConfig
+         */
+        public function createEncodingProfileWithInvalidWatermarkConfig(EncodingProfileConfig $encodingProfileConfig)
+        {
+            EncodingProfile::create($encodingProfileConfig);
+        }
 
-        EncodingProfile::create($encodingProfileConfig);
+        public function encodingProfileWithWatermarkConfigProvider()
+        {
+            $encodingProfileConfigs = array();
+            $croppingConfigs = array(
+                'watermarkWithNegativeTop'    => new WatermarkConfig(-10),
+                'watermarkWithNegativeRight'  => new WatermarkConfig(NULL, NULL, NULL, -1),
+                'watermarkWithNegativeLeft'   => new WatermarkConfig(NULL, NULL, -10, NULL),
+                'watermarkWithNegativeBottom' => new WatermarkConfig(NULL, -2, NULL, NULL),
+                'watermarkWithTopAndBottom'   => new WatermarkConfig(10, 2, NULL, NULL),
+                'watermarkWithNoImage'        => new WatermarkConfig(10, NULL, 2, NULL)
+            );
+
+            foreach ($croppingConfigs as $name => $watermarkConfig) {
+                $encodingProfileConfig = $this->encodingProfileProvider()['defaultEncodingProfileConfig'][0];
+                $encodingProfileConfig->watermarkConfig = $watermarkConfig;
+
+                $encodingProfileConfigs[$name] = array( $encodingProfileConfig );
+            }
+
+            return $encodingProfileConfigs;
+        }
+
     }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createWatermarkConfigWithNegativeRightValue()
-    {
-        Bitcodin::setApiToken($this->getApiKey());
-
-        $watermarkConfig = new WatermarkConfig();
-        $watermarkConfig->right = -1;
-
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->watermarkConfig = $watermarkConfig;
-
-        EncodingProfile::create($encodingProfileConfig);
-    }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createWatermarkConfigWithNegativeLeftValue()
-    {
-        Bitcodin::setApiToken($this->getApiKey());
-
-        $watermarkConfig = new WatermarkConfig();
-        $watermarkConfig->left = -10;
-
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->watermarkConfig = $watermarkConfig;
-
-        EncodingProfile::create($encodingProfileConfig);
-    }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createWatermarkConfigWithNegativeBottom()
-    {
-        Bitcodin::setApiToken($this->getApiKey());
-
-        $watermarkConfig = new WatermarkConfig();
-        $watermarkConfig->bottom = -2;
-
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->watermarkConfig = $watermarkConfig;
-
-        EncodingProfile::create($encodingProfileConfig);
-    }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createWatermarkConfigWithBottomAndTop()
-    {
-        Bitcodin::setApiToken($this->getApiKey());
-
-        $watermarkConfig = new WatermarkConfig();
-        $watermarkConfig->top = 10;
-        $watermarkConfig->bottom = 2;
-
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->watermarkConfig = $watermarkConfig;
-
-        EncodingProfile::create($encodingProfileConfig);
-    }
-
-    /**
-     * @test
-     * @expectedException               \bitcodin\exceptions\BitcodinException
-     */
-    public function createWatermarkConfigWithNoImage()
-    {
-        Bitcodin::setApiToken($this->getApiKey());
-
-        $watermarkConfig = new WatermarkConfig();
-        $watermarkConfig->top = 10;
-        $watermarkConfig->left = 2;
-
-        $encodingProfileConfig = $this->getEncodingProfileConfigTemplate();
-        $encodingProfileConfig->watermarkConfig = $watermarkConfig;
-
-        EncodingProfile::create($encodingProfileConfig);
-    }
-
-    private function getEncodingProfileConfigTemplate()
-    {
-        /* CREATE VIDEO STREAM CONFIG */
-        $videoStreamConfig = new VideoStreamConfig();
-        $videoStreamConfig->bitrate = 1024000;
-        $videoStreamConfig->height = 202;
-        $videoStreamConfig->width = 480;
-
-        /* CREATE AUDIO STREAM CONFIGS */
-        $audioStreamConfig = new AudioStreamConfig();
-        $audioStreamConfig->bitrate = 256000;
-
-        $encodingProfileConfig = new EncodingProfileConfig();
-        $encodingProfileConfig->name = $this->getName().'EncodingProfile';
-        $encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
-        $encodingProfileConfig->audioStreamConfigs[] = $audioStreamConfig;
-
-
-        return $encodingProfileConfig;
-    }
-}
