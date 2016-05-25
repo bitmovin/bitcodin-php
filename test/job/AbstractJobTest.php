a22a78475bc355f50a115f4eb765a70e3a77c99d
Gernot Zwantschko <gernot.zwantschko@bitmovin.net>
2016-05-25 08:55:26 +0200

    BitcodinApiTestBaseClass.php, AbstractJobTest.php: Replaced constructor with the recommended PHPUnit way to avoid unexpected testing behavior

--- test/job/AbstractJobTest.php
+++ test/job/AbstractJobTest.php
@@ -20,8 +20,9 @@ use DateTime;
 
 abstract class AbstractJobTest extends BitcodinApiTestBaseClass {
 
-    public function __construct() {
-        parent::__construct();
+    public function setUp()
+    {
+        parent::setUp();
     }
 
     /** HELPER METHODS **/
