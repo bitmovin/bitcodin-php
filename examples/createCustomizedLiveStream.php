e698747f39c48561137947dfdf7bb947091193e9
Gernot Zwantschko <gernot.zwantschko@bitmovin.net>
2016-07-27 14:46:02 +0200

    createCustomizedLiveStream.php: Example which uses a custom segment length and custom liveEdgeOffset for tuning the livestream end to end delay

---
+++ examples/createCustomizedLiveStream.php
@@ -0,0 +1,110 @@
+<?php
+
+    use bitcodin\AudioStreamConfig;
+    use bitcodin\Bitcodin;
+    use bitcodin\EncodingProfile;
+    use bitcodin\EncodingProfileConfig;
+    use bitcodin\LiveStream;
+    use bitcodin\Output;
+    use bitcodin\VideoStreamConfig;
+
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    Bitcodin::setApiToken('YOUR API KEY'); // Your can find your api key in the settings menu. Your account (right corner) -> Settings -> API
+
+    $namingPostfix = date("YmdHis");
+
+    /* CREATE ENCODING PROFILE FOR YOUR LIVE STREAM */
+    $encodingProfileConfig = new EncodingProfileConfig();
+    $encodingProfileConfig->name = 'FullHD+HD+SD Live Stream SL1 ';
+
+    $encodingProfileConfig->segmentLength = 1; //Custom segment length in seconds
+
+    /* CREATE VIDEO STREAM CONFIGS */
+    $videoStreamConfig = new VideoStreamConfig();
+    $videoStreamConfig->bitrate = 4800000;
+    $videoStreamConfig->width = 1920;
+    $videoStreamConfig->height = 1080;
+    $videoStreamConfig = new VideoStreamConfig();
+    $videoStreamConfig->bitrate = 2400000;
+    $videoStreamConfig->width = 1280;
+    $videoStreamConfig->height = 720;
+
+    $videoStreamConfig = new VideoStreamConfig();
+    $videoStreamConfig->bitrate = 1200000;
+    $videoStreamConfig->width = 854;
+    $videoStreamConfig->height = 480;
+
+    /* CREATE AUDIO STREAM CONFIGS */
+    $audio = new AudioStreamConfig();
+    $audio->bitrate = 128000;
+
+    $encodingProfileConfig->videoStreamConfigs[] = $videoStreamConfig;
+    $encodingProfileConfig->audioStreamConfigs[] = $audio;
+
+    /* CREATE ENCODING PROFILE */
+    $encodingProfile = EncodingProfile::create($encodingProfileConfig);
+
+    /* CREATE OUTPUT */
+    $outputConfig = new \bitcodin\S3OutputConfig();
+    $outputConfig->name = "Livestream Output Config";
+    $outputConfig->accessKey = "YOUR ACCESS KEY";
+    $outputConfig->secretKey = "YOUR SECRET KEY";
+    $outputConfig->bucket = "YOUR BUCKETNAME";
+    $outputConfig->prefix = "livestream" . $namingPostfix;
+    $outputConfig->makePublic = true;
+
+    $output = Output::create($outputConfig);
+
+    /* CREATE LIVE STREAM */
+    $livestreamName = "livestream-test-" . $namingPostfix;
+    $livestreamStreamKey = "livestreamtestone";
+    $timeShift = 30;
+    $liveEdgeOffset = 10;
+    $liveInstance = LiveStream::create($livestreamName, $livestreamStreamKey, $encodingProfile, $output, $timeShift, $liveEdgeOffset);
+
+    echo "Waiting until live stream is RUNNING...\n";
+    while ($liveInstance->status != $liveInstance::STATUS_RUNNING) {
+        sleep(2);
+        $liveInstance->update();
+        if ($liveInstance->status == $liveInstance::STATUS_ERROR) {
+            echo "ERROR occurred!";
+            throw new \Exception("Error occurred during Live stream creation");
+        }
+    }
+
+    echo "Livestream RTMP push URL: " . $liveInstance->rtmpPushUrl . "\n";
+    echo "Stream Key: " . $liveInstance->streamKey . "\n";
+    echo "MPD URL: " . $liveInstance->mpdUrl . "\n";
+    echo "HLS URL: " . $liveInstance->hlsUrl . "\n";
+
+    /*
+     * **************************************************
+     * AT THIS POINT YOU CAN STREAM TO YOUR LIVE STREAM *
+     * **************************************************
+     */
+
+    /*
+     * *********************************************************************************
+     * IMPORTANT! IF YOU HAVE FINISHED STREAMING DON'T FORGET TO DELETE YOUR LIVE STREAM
+     * *********************************************************************************
+     *
+     * Use something like the following to delete:
+     *
+
+        LiveStream::delete($liveInstance->id);
+
+        echo "Waiting until live stream is TERMINATED...\n";
+        while($liveInstance->status != $liveInstance::STATUS_TERMINATED)
+        {
+            sleep(2);
+            $liveInstance->update();
+            if($liveInstance->status == $liveInstance::STATUS_ERROR)
+            {
+                echo "ERROR occurred!";
+                throw new \Exception("Error occurred during Live stream deletion");
+            }
+        }
+
+        echo "Live stream TERMINATED\n";
+     */
